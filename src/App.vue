2d9d840f374bc77241a189c4c49369eb4ea0b309
Rolland <Rollandcampbell@msn.com>
2019-12-10 13:56:40 -0700

    adjusting imports

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+import Hello from "./components/Hello";
+
+export default {
+  name: "app",
+  components: {
+    Hello
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
